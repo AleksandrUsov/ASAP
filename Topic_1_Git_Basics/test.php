31e43d0c776bf4b893ec017c2069633de9ac7176
Aleksandr Usov <usov.als579@gmail.com>
2023-04-01 22:52:02 +0700

    'Улучшение' вывода рандомных чисел

--- Topic_1_Git_Basics/test.php
+++ Topic_1_Git_Basics/test.php
@@ -2,9 +2,11 @@
 
 echo "Hello world <br>";
 
-$arr = range(0, 100);
+$min = 0;
+$max = 200;
+$arr = range($min, $max);
 shuffle($arr);
 
-for ($i = 0; $i < 100; $i++) {
+for ($i = $min; $i < $max; $i++) {
 	echo array_shift($arr) . "<br>";
 }
